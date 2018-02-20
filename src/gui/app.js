d92e9cb93f13bf57d426a671d5d269ff1cd5a976
Muhammad Alam <alam@mmtutor.com>
2018-02-20 01:51:47 -0500

    start endState screen

--- src/gui/app.js
+++ src/gui/app.js
@@ -1,6 +1,9 @@
 import React from "react";
 import PlayerPanel from "./playerPanel.js";
 import OpponentsPanel from "./opponentsPanel";
+import TurnPanel from "./turnPanel.js";
+import PlayerInfo from "./playerInfo.js";
+import SmallCard from "./smallCard";
 
 class App extends React.Component {
   constructor(props) {
@@ -40,6 +43,29 @@ class App extends React.Component {
     </ul>
   );
 
+  renderReplay = () => {
+    const obj = this.state.history.slice().pop();
+    return (
+      <div>
+        <div>
+          <h1>{obj.winnerName} wins!</h1>
+          <TurnPanel turn={obj.endState.winningTurn} />
+          <div>
+            <PlayerInfo
+              name={obj.winnerName}
+              score={this.state.score[obj.winnerName]}
+            />
+            {createResult(obj.result[obj.winnerName])}
+          </div>
+        </div>
+        <div>
+          <h3>Remaining Cards:</h3>
+          {renderCardsLeft(obj.endState.lostCards)}
+        </div>
+      </div>
+    );
+  };
+
   render() {
     /// cleanup needed
     const p = this.props.player;
@@ -61,17 +87,50 @@ class App extends React.Component {
       </div>
     ) : showResultScreen ? (
       <div>
-        {renderReplay(this.state.history.slice().pop())}
-        <div>{this.state.history.slice().pop().winnerName} wins!</div>
-        <div>Scores:</div>
-        {this.renderScores()}
+        {this.renderReplay()}
         <button onClick={this.playMatch}>下一场比赛 Next Match</button>
       </div>
     ) : null;
   }
 }
-const renderReplay = obj => {
-  console.log(obj);
-  return JSON.stringify(obj);
+
+const createResult = num => {
+  const stilo = {
+    height: "2rem",
+    fontSize: "1.5rem",
+    color: num > 0 ? "green" : "red"
+  };
+  return <div style={stilo}>{num}</div>;
 };
+
+const renderCardsLeft = obj => {
+  const stilo = { display: "flex", marginLeft: "1rem", height: "4rem" };
+  const cardsLeftJSX = [];
+  for (let loser in obj) {
+    const item = (
+      <div key={loser}>
+        <div>{loser}</div>
+        <div style={stilo}>
+          {obj[loser].map(card => {
+            let uniqKey = Date.now().toString() + card._rank;
+            let selectionState = false;
+            return (
+              <div key={uniqKey}>
+                <SmallCard
+                  selected={selectionState}
+                  value={card.value}
+                  suit={card.suit}
+                />
+              </div>
+            );
+          })}
+          {createResult(-obj[loser].length)}
+        </div>
+      </div>
+    );
+    cardsLeftJSX.push(item);
+  }
+  return cardsLeftJSX;
+};
+
 export default App;
